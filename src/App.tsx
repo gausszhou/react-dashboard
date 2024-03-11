f428a01b9204cbd66c394ec3c05fadfeca2340ab
gausszhou <gausszhou@qq.com>
2024-03-11 22:58:42 +0800

    pages and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import Home from "./pages/home/Home"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },
+    {
+      path: "users",
+      element: <Users></Users>,
+    },
+    {
+      path: "products",
+      element: <Products></Products>,
+    },
+  ]);
+
 
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
